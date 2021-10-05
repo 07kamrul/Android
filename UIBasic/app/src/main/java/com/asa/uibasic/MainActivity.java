e414aa0cfefd4cc4f01c4a176824283f2bdcee1b
ASABD\kamrul <kamrul@asabd.org>
2021-10-05 11:54:30 +0600

    Toast apply

--- UIBasic/app/src/main/java/com/asa/uibasic/MainActivity.java
+++ UIBasic/app/src/main/java/com/asa/uibasic/MainActivity.java
@@ -3,22 +3,43 @@ package com.asa.uibasic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView txtHello;
+    private EditText editTxtName;
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnHello:
+                txtHello.setText("Hello " + editTxtName.getText().toString());
+                break;
+            case R.id.editTextName:
+                Toast.makeText(this, "Attempting to type something", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onHelloBtnClicked(View view) {
-        TextView txtWelcome = findViewById(R.id.txtWelcome);
-        txtWelcome.setText("Hello Again");
+        Button btnHello = findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(this);
 
+        editTxtName = findViewById(R.id.editTextName);
+        editTxtName.setOnClickListener(this);
+        
+        txtHello = findViewById(R.id.txtHello);
     }
 }
